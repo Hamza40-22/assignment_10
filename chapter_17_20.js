9b16381513e7a44906f49cd2d3957bd8ee5806c3
Hamza40-22 <95353930+Hamza40-22@users.noreply.github.com>
2023-09-23 03:37:51 +0500

    Add files via upload

---
+++ chapter_17_20.js
@@ -0,0 +1,143 @@
+// Question # 1
+
+// var multi_universe=[[],[],[]]
+
+
+// ***************************************************     *****************************************
+
+
+// Question # 2
+
+
+// var multi_matrix=[[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// document.write(multi_matrix)
+
+
+// ***************************************************     *****************************************
+
+// Question # 3
+
+
+
+// for(let i=1;i<=10;i++){
+//     document.write(i)
+//     document.write("<br>")
+// }
+
+
+
+// ***************************************************     *****************************************
+
+// Question # 4
+
+// var multi_table=+prompt("Which table you want to print?")
+// var length=+prompt("Enter your desired length ")
+// for (let i=1;i<=length;i++){
+//     document.write(`${multi_table} x ${i} = `,multi_table*i)
+//     document.write("<br>");
+// }
+
+
+
+// ***************************************************     *****************************************
+
+// Question # 5
+
+// var fruits = ["apple","banana","mango","orange","strawberry"]
+// for (var i=0;i<fruits.length;i++){
+//     document.write(fruits[i])
+//     document.write("<br>")
+// }
+
+
+
+// ***************************************************     *****************************************
+
+// Question # 6
+
+// document.write("a) Counting:")
+// for (var i=1;i<=15;i++){
+//     document.write(`${i} , `)
+// }
+// document.write("<br>");
+// document.write("<br>")
+// document.write("b) Reverse Counting:")
+// for (var i = 10; i >= 1; i--) {
+//     document.write(`${i} , `);
+// }
+// document.write("<br>")
+// document.write("<br>")
+// document.write("c) Even:")
+// for(var i=0;i<=20;i++){
+//     if(i%2==0){
+//         document.write(`${i} , `);  
+//     }
+// }
+// document.write("<br>")
+// document.write("<br>")
+// document.write("d) Odd:")
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 !== 0) {
+//     document.write(`${i} , `);
+//     }
+// }
+// document.write("<br>")
+// document.write("<br>")
+// document.write("e) Series:")
+// for (var i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//     document.write(`${i}K , `);
+//     }
+// }
+
+
+
+// ***************************************************     *****************************************
+
+// Question # 8
+
+
+// var arr = [24, 53, 78, 91, 12];
+// document.write("Array Items ", arr);
+// document.write("<br>");
+// let largest = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > largest) {
+//         smallest = arr[i];
+//     }
+// }
+// document.write("The largest number is ", largest);
+
+
+// ***************************************************     *****************************************
+
+// Question # 9
+
+
+// var arr = [24, 53, 78, 91, 12];
+// document.write("Array Items ", arr);
+// document.write("<br>");
+// let smallest = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < smallest) {
+//         smallest = arr[i];
+//     }
+// }
+// document.write("The smallest number is ", smallest);
+
+
+
+// ***************************************************     *****************************************
+
+// Question # 10
+
+
+// for(var i=1;i<=100;i++){
+//     if(i%5==0){
+//         document.write(i )
+//         document.writeln("<br>")
+//     }
+// }
+
+
+
